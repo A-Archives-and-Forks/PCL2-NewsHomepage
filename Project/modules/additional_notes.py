1b0b7176799f38ad7f45c7a42725785fa52ac4b8
Nattiden <2682645990@qq.com>
2026-03-26 00:01:29 +0800

    added translator note

---
+++ Project/modules/additional_notes.py
@@ -0,0 +1,8 @@
+"""
+向 MARKDOWN 解析器注册开发者与译者注释类型
+"""
+
+from homepagebuilder.interfaces import require
+markdown_parsers = require('markdown_parsers')
+markdown_parsers.QUOTE_TYPE_NAMES["devnote"] = '开发者注释'
+markdown_parsers.QUOTE_TYPE_NAMES["translatornote"] = '译者注'
